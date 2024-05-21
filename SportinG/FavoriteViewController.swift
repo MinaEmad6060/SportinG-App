17e1271e4e13acec2472d4db30298c38c9995398
Rawan Elsayed <78448934+Rawan78@users.noreply.github.com>
2024-05-21 14:09:29 +0300

    add splash screen and home and favorite screens

---
+++ SportinG/FavoriteViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteViewController.swift
+//  SportinG
+//
+//  Created by Rawan Elsayed on 20/05/2024.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ SportinG/SplashScreenController.swift
@@ -0,0 +1,21 @@
+//
+//  SplashScreenController.swift
+//  SportinG
+//
+//  Created by Rawan Elsayed on 20/05/2024.
+//
+
+import UIKit
+
+class SplashScreenController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Wait 2 seconds and go to splash screen
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3){
+            self.performSegue(withIdentifier: "OpenSplash", sender: nil)
+        }
+    }
+
+}
