3404d8ffc6097b954bf28e67c41b2e67b8acf772
MinaEmad6060 <minaemad6060@gmail.com>
2024-05-22 13:26:36 -0700

    implement method (formatUrl) to call any endpoint from api

--- SportinG/Modules/Home/LeagueDetVC.swift
+++ SportinG/Modules/Home/LeagueDetVC.swift
@@ -71,7 +71,8 @@ class LeagueDetVC: UIViewController, UICollectionViewDelegate, UICollectionViewD
                 self?.leagueDetailsCollectionView.reloadData()
             }
         })
-        fetchDataFromAPi?.getFootBallData (url: Utils.Urls.FootBall_League_Teams.rawValue,handler: {[weak self] teamsMatches in
+        //Utils.Urls.FootBall_League_Teams.rawValue
+        fetchDataFromAPi?.getFootBallData (url: fetchDataFromAPi?.formatURL(sport: "football", met: "Teams", teamId: "", leagueId: "152") ?? "",handler: {[weak self] teamsMatches in
             DispatchQueue.main.async {
                 self?.teamsResults = teamsMatches.result
                 print("ColView: \(teamsMatches.result.count)")
@@ -270,47 +271,12 @@ class LeagueDetVC: UIViewController, UICollectionViewDelegate, UICollectionViewD
         }
     }
     
-    // MARK: UICollectionViewDelegate
-    
-    /*
-     // Uncomment this method to specify if the specified item should be highlighted during tracking
-     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
-    
-    /*
-     // Uncomment this method to specify if the specified item should be selected
-     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
-    
-    /*
-     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-     
-     }
-     */
+  
     
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnFav(_ sender: Any) {
+        
     }
-    */
+    
 
 }
--- SportinG/Services/NetworkService/FetchDataFromApi.swift
+++ SportinG/Services/NetworkService/FetchDataFromApi.swift
@@ -10,6 +10,16 @@ import Alamofire
 
 
 class FetchDataFromApi{
+    
+    var baseUrl = "https://apiv2.allsportsapi.com/"
+    var apiKey = "22ad8dd732a55a3fe4d2f4df34998396b28f2b23f9020add2c4c977342017644"
+    var rangeOfUpcomingEvents = "&from=2024-06-20&to=2024-06-20"
+    
+    func formatURL(sport: String, met: String, teamId: String, leagueId: String) -> String{
+        return baseUrl+sport+"/?met="+met+"&APIkey="+apiKey+"&leagueId="+leagueId+"&teamId"+rangeOfUpcomingEvents
+    }
+    
+    
     func getFootBallData(url: String, handler: @escaping (SportDetails)->Void){
         let urlFB = URL(string: url)
         print("Loading")
