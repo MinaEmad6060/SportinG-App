36f614df99e3822c69a7a0ffc0a51c6f14d69b7d
MinaEmad6060 <minaemad6060@gmail.com>
2024-05-23 14:15:38 -0700

    update in class FetchDataFromApi

--- SportinG/Services/NetworkService/FetchDataFromApi.swift
+++ SportinG/Services/NetworkService/FetchDataFromApi.swift
@@ -13,10 +13,8 @@ class FetchDataFromApi{
     
     var baseUrl = "https://apiv2.allsportsapi.com/"
     var apiKey = "22ad8dd732a55a3fe4d2f4df34998396b28f2b23f9020add2c4c977342017644"
-//    static var sport = ""
     
     func formatURL(sport: String, met: String, teamId: String="", leagueId: String="") -> String{
-//        FetchDataFromApi.sport = sport
         return baseUrl+sport+"/?met="+met+"&APIkey="+apiKey+"&leagueId="+leagueId+"&teamId="+teamId
     }
     
