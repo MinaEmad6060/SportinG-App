8ab0a997c2985f1daf795ec1bff9e0ab668939e4
MinaEmad6060 <minaemad6060@gmail.com>
2024-05-28 09:23:56 -0700

    edit in UI of LeagueCollectionViewCell

--- SportinG/Utils/LeagueDetailsCollectionViewCell.swift
+++ SportinG/Utils/LeagueDetailsCollectionViewCell.swift
@@ -24,32 +24,17 @@ class LeagueDetailsCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var scoreOfLiveMatch: UILabel!
     
     
-    
-    
-    
-    
-    
-    
-//    @IBOutlet weak var homeImage: UIImageView!
-//    
-//    @IBOutlet weak var homeTeam: UILabel!
-//    
-//    @IBOutlet weak var awayImage: UIImageView!
-//    
-//    @IBOutlet weak var awayTeam: UILabel!
-//    
-//    
-//    @IBOutlet weak var dateOfMatch: UILabel!
-//    
-//    
-//    @IBOutlet weak var timeOfMatch: UILabel!
-//    
-//    
-//    @IBOutlet weak var scoreOfLiveMatch: UILabel!
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.contentView.layer.cornerRadius = 30.0
+        self.contentView.layer.masksToBounds = false
+        self.contentView.clipsToBounds = true
+        self.contentView.backgroundColor = UIColor(red: 0.5, green: 0.25, blue: 0.25, alpha: 0.1)
+
+        // Optional: Set border color and width
+        self.contentView.layer.borderColor = UIColor.red.cgColor
+        self.contentView.layer.borderWidth = 2.0
     }
 
 }
